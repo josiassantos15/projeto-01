12ac06630489a7c6315f6e00ecc1b5491e704e49
Josias Santos <josiassantos1577@gmail.com>
2022-06-07 22:36:12 -0300

    terceiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { RepositoryList } from './components/RepositoryList';
+import './styles/global.scss';
+
 export function App() {
-  throw new Error('Eita, volta que deu ruim');
-  
-  return <h1>Hello World!</h1>
+  return <RepositoryList/>
 }
